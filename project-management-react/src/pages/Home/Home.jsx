37031f87729ffb508569bc800ecf933436fea2ed
AnSecret24 <thaoan19102004@gmail.com>
2025-09-18 12:40:23 +0700

    first commit

---
+++ project-management-react/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import ProjectList from "../ProjectList/ProjectList";
+import React from "react";
+ const Home  = ()   => {
+    return (
+        <div>
+              <ProjectList/>
+        </div>
+    )
+ }
+
+ export default Home
