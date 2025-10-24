0a2b673f66cac3d0d28e6cb087b1168eb133dd78
Levanbinh123 <levanbin1712@gmail.com>
2025-10-24 17:07:30 +0700

    asa

--- project-management-react/vite.config.js
+++ project-management-react/vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
